22d933efe6f751ae38e351d08c1dd41c45babeeb
Anirban Dutta <anirbandutta428@gmail.com>
2023-04-12 20:36:01 +0530

    Delete db directory

--- db/connection.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const DB = process.env.MONGO_URL;
-
-mongoose.connect(
-    DB
-), { useNewUrlParser: true };
-console.log(mongoose.connection.readyState);
